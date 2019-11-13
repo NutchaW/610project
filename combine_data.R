4aa223f4e2ca3a93df38a87b84923718dac93eb5
Nutcha <ahctun_woon@1x-nat-vl930-172-30-124-44.wireless.umass.edu>
2019-11-13 11:26:43 -0500

    more data frames at different level

--- combine_data.R
+++ combine_data.R
@@ -8,3 +8,7 @@ dattest<-ilinet(region=c("state"),years = NULL)
 # this is at a national level....what happens when we have different information at different levels of hierarchy?
 agedis<-age_group_distribution(years=NULL)
 # loaded rnoaa in case we need humidity data
+#### different geographic level....
+dattest1<-ilinet(region=c("national"),years = NULL)
+dattest2<-ilinet(region=c("census"),years = NULL)
+dattest3<-ilinet(region=c("hhs"),years = NULL)
